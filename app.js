e4327385a421885726fff5154bc713b1701e6330
Thibault CORRIHONS <thibault.corrihons.ext@orange.com>
2022-05-05 14:47:00 +0200

    Adding CORS to express

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const cors = require('cors');
+app.use(cors());
+
 let router = require('./router');
 let userController = require('./api/userController');
 
